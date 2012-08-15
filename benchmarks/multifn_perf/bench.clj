56b5fe26b98f35bae35fec79d28d221040d184a9
David Santiago <david.santiago@gmail.com>
2012-08-14 21:29:45 -0700

    Add benchmark cases for inline and extended protocols, just for comparison.

--- benchmarks/multifn_perf/bench.clj
+++ benchmarks/multifn_perf/bench.clj
@@ -3,14 +3,27 @@
 
 (defgoal contended-mfn "A multi-threaded high-contention multifn.")
 
+;; Multimethod version
 (defmulti add-something even?)
 (defmethod add-something true [x] (+ x 0))
 (defmethod add-something false [x] (+ x 1))
 
+;; Regular function version
 (defn add-something-fn [x]
   (if (even? x)
     (+ x 0)
     (+ x 1)))
+
+;; Protocol versions
+(defprotocol SomethingAddable
+  (add-something-prot [this x]))
+(defrecord InlineRec []
+  SomethingAddable
+  (add-something-prot [r x] (if (even? x) (+ x 0) (+ x 1))))
+(defrecord ExtendedRec [])
+(extend-type ExtendedRec
+  SomethingAddable
+  (add-something-prot [r x] (if (even? x) (+ x 0) (+ x 1))))
 
 (defn work []
   (loop [acc 0
@@ -18,6 +31,14 @@
     (if (= 0 cntr)
       acc
       (recur (+ acc (add-something cntr))
+             (dec cntr)))))
+
+(defn work-prot [rec]
+  (loop [acc 0
+         cntr 2000000]
+    (if (= 0 cntr)
+      acc
+      (recur (+ acc (add-something-prot rec cntr))
              (dec cntr)))))
 
 (defcase contended-mfn :regular-fn
@@ -45,3 +66,13 @@
         f3 (future (work))
         f4 (future (work))]
     (+ @f1 @f2 @f3 @f4)))
+
+(defcase contended-mfn :inline-protocol
+  []
+  (let [rec (->InlineRec)]
+    (work-prot rec)))
+
+(defcase contended-mfn :extended-protocol
+  []
+  (let [rec (->ExtendedRec)]
+    (work-prot rec)))
